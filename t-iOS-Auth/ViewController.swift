6c77de27265a4657d1a95b1f3b1a75924cdd31cf
Lorenzo Hernandez <lorenzohernandez@Lorenzos-MacBook-Air.local>
2019-01-01 17:16:04 -0800

    Initial Commit

---
+++ t-iOS-Auth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  t-iOS-Auth
+//
+//  Created by Lorenzo Hernandez on 1/1/19.
+//  Copyright © 2019 Lorenzo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
