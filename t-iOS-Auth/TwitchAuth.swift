279d7eff48c69d6629975fb2ca9895f6d60eefef
Lorenzo Hernandez <lorenzohernandez@Lorenzos-MacBook-Air.local>
2019-01-01 19:57:54 -0800

    setting up keys

--- t-iOS-Auth/TwitchAuth.swift
+++ t-iOS-Auth/TwitchAuth.swift
@@ -11,25 +11,54 @@ import UIKit
 class TwitchAuth: UIButton {
     var width : CGFloat = 80
     var height : CGFloat = 40
+    var url : String = "https://id.twitch.tv/oauth2/authorize"
+    var credentials : Dictionary<String, String>?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let old_center = self.center
-        self.frame = CGRect(x: self.frame.minX, y: self.frame.minY, width: width * 2, height: height * 2)
-        self.setTitle("", for: .normal)
-        self.imageView?.contentMode = .scaleAspectFit
-        self.setBackgroundImage(UIImage(named: "twitch_btn_img"), for: .normal)
-        self.center = old_center
-        self.imageView?.center = self.center
+        self.setupUI()
+        
+        
+        if let path = Bundle.main.path(forResource: "keys", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let json = jsonResult as? Dictionary<String, String>{
+                    self.credentials = json
+                }
+            } catch {
+                // handle error
+            }
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        let old_center = self.center
+        self.setupUI()
+        
+        if let path = Bundle.main.path(forResource: "keys", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let json = jsonResult as? Dictionary<String, String>{
+                    self.credentials = json
+                }
+            } catch {
+                // handle error
+            }
+        }
+        
+    }
+    
+    private func setupUI(){
+        // setting up button image
         self.frame = CGRect(x: self.frame.minX, y: self.frame.minY, width: width * 2, height: height * 2)
         self.setTitle("", for: .normal)
         self.setBackgroundImage(UIImage(named: "twitch_btn_img"), for: .normal)
-        self.center = old_center
         self.imageView?.center = self.center
+    }
+    
+    private func startAuth(sender: UIButton!){
+    
     }
 }
