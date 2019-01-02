0fa5826b8fa5bca690d358f83951261e98daf3b3
Lorenzo Hernandez <lorenzohernandez@Lorenzos-MacBook-Air.local>
2019-01-01 17:38:23 -0800

    test area controller

---
+++ t-iOS-Auth/TestController.swift
@@ -0,0 +1,18 @@
+//
+//  TestController.swift
+//  t-iOS-Auth
+//
+//  Created by Lorenzo Hernandez on 1/1/19.
+//  Copyright © 2019 Lorenzo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
