6cef48607680d7b76c7874785098a4c69424162d
Lorenzo Hernandez <lorenzohernandez@Lorenzos-MacBook-Air.local>
2019-01-01 17:36:37 -0800

    Starting with auth controller

---
+++ t-iOS-Auth/WVController.swift
@@ -0,0 +1,32 @@
+//
+//  WKWVauth.swift
+//  Lobot-iOS
+//
+//  Created by Lorenzo Hernandez on 12/5/18.
+//  Copyright © 2018 Lorenzo Hernandez. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WKWVauth: UIViewController {
+    var webView: WKWebView!
+    var targetURL: URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func handle(_ url: URL) {
+        targetURL = url
+        super.handle(url)
+        loadAddressURL()
+    }
+    
+    func loadAddressURL() {
+        guard let url = targetURL else { return }
+        let req = URLRequest(url: url)
+        
+        self.webView?.load(req)
+    }
+}
